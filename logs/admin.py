3f5dbf6ff99dbafa23be8b544dd6123cb804da1b
Corey Farwell <Corey Farwell>
2011-03-30 13:00:10 -0700

    changed columns to match new models

--- logs/admin.py
+++ logs/admin.py
@@ -2,6 +2,6 @@ from django.contrib.gis import admin
 from models import Incident
 
 class IncidentAdmin(admin.GeoModelAdmin):
-    list_display = ('name', 'eventType', 'location', 'time', 'city')
+    list_display = ('name', 'category', 'jrsdtn', 'time')
 
 admin.site.register(Incident, IncidentAdmin)
