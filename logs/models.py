fefdce50bf909be88ecd16159f9ea6afd0ff3c1d
Corey Farwell <Corey Farwell>
2011-03-30 12:58:34 -0700

    a whole bunch more models for data

--- logs/models.py
+++ logs/models.py
@@ -2,10 +2,9 @@ from django.contrib.gis.db import models
 
 class Incident(models.Model):
     name = models.CharField(max_length=20)
-    typeS = models.CharField(max_length=6)
-    typeL = models.CharField(max_length=50)
+    type = models.CharField(max_length=6)
     details = models.CharField(max_length=50)
-    category = models.CharField(max_length=10)
+    category = models.CharField(max_length=15)
     time = models.DateTimeField()
     latlng = models.PointField()
     address = models.CharField(max_length=100)
