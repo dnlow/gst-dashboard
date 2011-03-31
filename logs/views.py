93f3cb42cee5055db17372602629dcf4f050b3d6
Corey Farwell <Corey Farwell>
2011-03-30 22:43:43 -0700

    fixed view, looking better, but crappy

--- logs/views.py
+++ logs/views.py
@@ -3,4 +3,6 @@ from models import Incident
 
 def incidentInfo(request, incident_id):
     incident = Incident.objects.get(name=incident_id)
-    return HttpResponse(incident.name + " - " + str(incident.location))
+    response = incident.name + " - " + str(incident.latlng)
+    response += "<br /><hr /><br />" + incident.log
+    return HttpResponse(response)
