ac7e92e44a9d5468112b6f4996745a1fbfd262af
Corey Farwell <Corey Farwell>
2011-04-07 14:16:06 -0700

    implement strip command

---
+++ logs/management/commands/strip.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from fnmatch import fnmatch
+import os
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Removes unhelpful entries from the logs'
+
+    def handle(self, *args, **options):
+        for log in os.listdir('data/raw/'):
+            if fnmatch(log, '*.txt'):
+                raw = open('/home/cfadmin/corey/DjangoCAD/data/raw/' + log, 'r+')
+                stripped = open('/home/cfadmin/corey/DjangoCAD/data/stripped/' + log, 'w')
+                for line in raw.readlines():
+                    fields = line.split('|')
+                    if len(fields) > 9 and fields[7] and fields[8]:
+                        stripped.write(line)
+                raw.close()
+                stripped.close()
