33701882ac6f53d991e20c5ae49350b7a0cdefe9
Corey Farwell <coreyf@rwell.org>
2011-04-27 11:22:44 -0700

    made directories global

--- logs/management/commands/strip.py
+++ logs/management/commands/strip.py
@@ -7,10 +7,10 @@ class Command(BaseCommand):
     help = 'Removes unhelpful entries from the logs'
 
     def handle(self, *args, **options):
-        for log in os.listdir('/home/cfadmin/corey/DjangoCAD/data/raw/'):
+        for log in os.listdir('data/raw/'):
             if fnmatch(log, '*.txt'):
-                raw = open('/home/cfadmin/corey/DjangoCAD/data/raw/' + log, 'r+')
-                stripped = open('/home/cfadmin/corey/DjangoCAD/data/stripped/' + log, 'w')
+                raw = open('data/raw/' + log, 'r+')
+                stripped = open('data/stripped/' + log, 'w')
                 for line in raw.readlines():
                     fields = line.split('|')
                     if len(fields) > 9 and fields[7] and fields[8]:
