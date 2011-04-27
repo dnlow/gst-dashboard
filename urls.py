00c073a2a64a140845b4d57fc0b01b97a54beeff
Corey Farwell <Corey Farwell>
2011-04-27 11:00:03 -0700

    reflect new views

--- urls.py
+++ urls.py
@@ -6,6 +6,8 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     (r'^incident/(?P<incident_id>\w+)/$', 'logs.views.incidentInfo'),
+    (r'^viewer/$', 'logs.views.viewer'),
     (r'^feed/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feed_dict}),
+    (r'^json/$', 'logs.views.json_incident'),
     (r'^admin/', include(admin.site.urls)),
 )
