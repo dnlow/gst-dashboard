2a9f053ef410ea67c87912c65c782c48e4789a78
Corey Farwell <Corey Farwell>
2011-04-27 11:00:22 -0700

    add new views

--- logs/views.py
+++ logs/views.py
@@ -1,8 +1,23 @@
-from django.http import HttpResponse
 from models import Incident
+from django.shortcuts import render
+from django.utils import simplejson
+from django.http import HttpResponse
 
 def incidentInfo(request, incident_id):
     incident = Incident.objects.get(name=incident_id)
-    response = incident.name + " - " + str(incident.latlng)
-    response += "<br /><hr /><br />" + incident.log
-    return HttpResponse(response)
+    return render(request, 'incident.html', {"incident": incident})
+
+def viewer(request):
+    incidents = Incident.objects.order_by('-time')[:100]
+    return render(request, 'viewer.html', {"incidents": incidents})
+
+def json_incident(request):
+    incidents = Incident.objects.all()
+    return HttpResponse(incidents.geojson().all()[0].latlng.geojson, mimetype='application/json')
+    '''
+    result = []
+    incidents = Incident.objects.order_by('-time')[:100]
+    for i in incidents:
+        result.append({'id':i.name, 'jrsdtn':i.jrsdtn, 'category':i.category})
+    return HttpResponse(simplejson.dumps(result), mimetype='application/json')
+    '''
