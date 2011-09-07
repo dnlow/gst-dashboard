9273e96297e7fb40feab8935189ee5fb88c2f0a3
Corey Farwell <coreyf@rwell.org>
2011-09-07 01:13:59 +0000

    renamed

--- logs/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.gis import admin
-from models import Incident
-
-class IncidentAdmin(admin.OSMGeoAdmin):
-    list_display = ('name', 'category', 'details', 'jrsdtn', 'time')
-    list_filter = ('category', 'jrsdtn', 'time')
-
-admin.site.register(Incident, IncidentAdmin)
--- urls.py
+++ urls.py
@@ -5,8 +5,8 @@ from logs.feeds import feed_dict
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^incident/(?P<incident_id>\w+)/$', 'logs.views.incidentInfo'),
-    (r'^viewer/$', 'logs.views.viewer'),
+    #(r'^incident/(?P<incident_id>\w+)/$', 'logs.views.incidentInfo'),
+    #(r'^viewer/$', 'logs.views.viewer'),
     (r'^feed/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feed_dict}),
     (r'^json/$', 'logs.views.json_incident'),
     (r'^json/(?P<number>\d+)/$', 'logs.views.json_incident'),
