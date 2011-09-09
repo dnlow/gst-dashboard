8a390ea12123a84b044192eb74d86f833f068463
frewsxcv <coreyf@rwell.org>
2011-09-08 20:12:28 -0700

    import all the views, reordered geojson, all feeds are in feed/

--- urls.py
+++ urls.py
@@ -1,14 +1,16 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from logs.feeds import feed_dict
+from django.contrib.syndication.views import feed
+from incidents.feeds import feed_dict
+from incidents.views import json_incident
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #(r'^incident/(?P<incident_id>\w+)/$', 'logs.views.incidentInfo'),
-    #(r'^viewer/$', 'logs.views.viewer'),
-    (r'^feed/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feed_dict}),
-    (r'^json/$', 'logs.views.json_incident'),
-    (r'^json/(?P<number>\d+)/$', 'logs.views.json_incident'),
+    #(r'^incident/(?P<incident_id>\w+)/$', 'incidents.views.incidentInfo'),
+    #(r'^viewer/$', 'incidents.views.viewer'),
+    (r'^feed/geojson/$', json_incident),
+    (r'^feed/geojson/(?P<number>\d+)/$', json_incident),
+    (r'^feed/(?P<url>.*)/$', feed, {'feed_dict': feed_dict}),
     (r'^admin/', include(admin.site.urls)),
 )
