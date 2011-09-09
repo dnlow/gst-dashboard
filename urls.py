6b495149b40efd4b8db3329670ba3473f88b645e
Corey Farwell <coreyf@rwell.org>
2011-09-09 06:55:18 +0000

    fixed urls.py feed redirection

--- urls.py
+++ urls.py
@@ -9,8 +9,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
     #(r'^incident/(?P<incident_id>\w+)/$', 'incidents.views.incidentInfo'),
     #(r'^viewer/$', 'incidents.views.viewer'),
-    (r'^feed/geojson/$', json_incident),
-    (r'^feed/geojson/(?P<number>\d+)/$', json_incident),
-    (r'^feed/(?P<url>.*)/$', feed, {'feed_dict': feed_dict}),
+    (r'^feed/geojson$', json_incident),
+    (r'^feed/geojson/(?P<number>\d+)$', json_incident),
+    (r'^feed/(?P<url>.*)$', feed, {'feed_dict': feed_dict}),
     (r'^admin/', include(admin.site.urls)),
 )
