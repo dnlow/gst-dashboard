0320fb60805a6b404f0894262fb7152eeae8d7b7
Corey Farwell <coreyf@rwell.org>
2011-10-24 16:18:41 +0000

    viewer is working again, added staticURL variable

--- urls.py
+++ urls.py
@@ -3,12 +3,14 @@ from django.contrib import admin
 from django.contrib.syndication.views import feed
 from incidents.feeds import feed_dict
 from incidents.views import json_incident
+from dashboard.views import dashboard
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     #(r'^incident/(?P<incident_id>\w+)/$', 'incidents.views.incidentInfo'),
-    #(r'^viewer/$', 'incidents.views.viewer'),
+    (r'^viewer/$', 'viewer.views.viewer'),
+    (r'^dashboard/', dashboard),
     (r'^feed/geojson$', json_incident),
     (r'^feed/geojson/(?P<number>\d+)$', json_incident),
     (r'^feed/(?P<url>.*)$', feed, {'feed_dict': feed_dict}),
