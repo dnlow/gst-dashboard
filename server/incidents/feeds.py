88330a371f96d415c4f3bc9fd782fede38fb2e0a
Corey Farwell <coreyf@rwell.org>
2012-03-13 20:43:35 +0000

    changed urls to reflect new domain

--- server/incidents/feeds.py
+++ server/incidents/feeds.py
@@ -3,20 +3,20 @@ from models import Incident
 
 class CADGeoRSS(Feed):
     title = "San Luis Obispo Incident Data"
-    link = "http://cfslo.selfip.org/"
+    link = "http://dashboard.slocountyfire.org/"
     description = "Latest 100 incidents"
     
     def items(self):
         return Incident.objects.order_by('-time')[:100]
         
     def item_title(self, item):
-        return item.name
+        return item.event_id
 
     def item_description(self, item):
         return "%s @ %s(%s)" % (item.details, item.jrsdtn, item.time)
 
     def item_link(self, item):
-        return "http://cfslo.selfip.org:9000/incident/%s" % item.name
+        return "http://dashboard.slocountyfire.org/incident/%s" % item.event_id
  
     def item_geometry(self, item):
         return item.latlng
