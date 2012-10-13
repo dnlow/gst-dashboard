daac9554de3c1a746ec08b674c3603aa0be9c8ef
Corey Farwell <coreyf@rwell.org>
2012-10-12 22:55:47 -0700

    fix rss

--- server/urls.py
+++ server/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from django.contrib.syndication.views import feed
-from incidents.feeds import feed_dict
+from incidents.feeds import CADGeoRSS
 from incidents.views import json_incident
 from dashboard.views import dashboard
 from viewer.views import viewer
@@ -9,12 +8,11 @@ from viewer.views import viewer
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #(r'^incident/(?P<incident_id>\w+)/$', 'incidents.views.incidentInfo'),
     (r'^$', dashboard),
     (r'^oldviewer/$', viewer),  # old viewer that is no longer used
     (r'^dashboard/', dashboard),
     (r'^feed/geojson$', json_incident),
     (r'^feed/geojson/(?P<number>\d+)$', json_incident),
-    (r'^feed/(?P<url>.*)$', feed, {'feed_dict': feed_dict}),
+    (r'^feed/rss$', CADGeoRSS()),
     (r'^admin/', include(admin.site.urls)),
 )
