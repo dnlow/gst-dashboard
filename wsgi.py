bdfa7660b54ffcd3895b7bf285b0ca5c006187ea
Corey Farwell <coreyf@rwell.org>
2013-01-10 03:08:35 +0000

    remove celery

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,7 @@
 import os
 
 import django.core.handlers.wsgi
-import djcelery
 
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'gst_dashboard.settings'
-djcelery.setup_loader()
 application = django.core.handlers.wsgi.WSGIHandler()
