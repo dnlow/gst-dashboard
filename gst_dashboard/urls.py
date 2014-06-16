642b30b9852ac60a9da00bb6fe94c5606542139c
Corey Farwell <coreyf@rwell.org>
2014-06-16 18:27:14 +0000

    Use the GIS admin instead of builtin admin

--- gst_dashboard/urls.py
+++ gst_dashboard/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from django.contrib import admin
+from django.contrib.gis import admin
 
 from gst_dashboard.incidents.feeds import IncidentFeed
 from gst_dashboard.incidents.views import json_incident
